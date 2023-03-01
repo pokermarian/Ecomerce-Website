7d7b1e3bc62311b316275e5da20135d77a6f2953
Marian <pokermarian@gmail.com>
2023-03-01 13:01:14 +0200

    navbar created and setup react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
+import {ChakraProvider} from '@chakra-ui/react' ;
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom' ;
+import Navbar from './components/Navbar';
+
 function App() {
   return (
-    <div>
-      <h1>Our App</h1>
-    </div>
+    <ChakraProvider>
+      <Router>
+        <Navbar />
+        <main>
+          {/* <Routes></Routes>
+          <Routes></Routes>
+          <Routes></Routes> */}
+        </main>
+      </Router>
+    </ChakraProvider>
   );
 }
 
